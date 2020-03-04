6acb8bb233e0a11469301f9f5c7319bf169321fd
dylanm2094 <59807851+dylanm2094@users.noreply.github.com>
2020-03-03 20:33:33 -0500

    fixed avg delta

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,6 +10,7 @@ p_l_t = 0
 prev_p_l = 867884
 p_l_min = 0
 p_l_max = 0
+sum_delta = 0
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -19,6 +20,7 @@ with open(csvpath) as csvfile:
         months += 1
         p_l_t += int(row[1])
         delta = int(row[1]) - prev_p_l
+        sum_delta += delta
         prev_p_l = int(row[1])
         if delta >= p_l_max:
             p_l_max = delta
@@ -29,7 +31,7 @@ with open(csvpath) as csvfile:
         else:
             continue
     
-    avg_delta = round(delta / (months - 1),2)
+    avg_delta = round(sum_delta / (months - 1),2)
     output = ["Financial Analysis\n","----------------------------\n",f"Total Months: {months}\n",f"Total: ${p_l_t}\n",f"Average  Change: ${avg_delta}\n",f"Greatest Increase in Profits: {max_mon} (${p_l_max})\n",f"Greatest Decrease in Profits: {min_mon} (${p_l_min})\n"]
 
     print("Financial Analysis")
